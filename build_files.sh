c954a00afbbebc24fb4cbace93834985a2247681
Franklin P. Ramos <franky88@users.noreply.github.com>
2023-07-28 10:27:38 +0800

    updates

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,6 @@
 echo " BUILD STARTING... "
 python3.9 -m pip install -r requirements.txt
+python3.9 manage.py migrate --clear
+python3.9 manage.py makemigrations store --clear
 python3.9 manage.py collectstatic --noinput --clear
 echo " BUILD ENDING "
